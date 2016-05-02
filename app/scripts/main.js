8020f87b08f06432d29f6fe9e3b110acba7523b3
Whitney Williams <wweliz@gmail.com>
2016-05-02 13:48:58 -0400

    fix spinner animation for GitHub and Twitter logos at bottom of page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -144,3 +144,8 @@ $(window).resize(function() {
 	centerAboutText();
 	make2Columns();
 });
+
+// TWITTER AND GITHUB LOGO ANIMATION /////////////////////////////////////
+$('.icon').bind('mouseenter mouseleave', function() {
+  $(this).toggleClass('animated');
+});
